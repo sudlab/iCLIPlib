a1145bbfb384edf152ff3fecd5462685440d79bc
IanSudbery <ian.sudbery@dpag.ox.ac.uk>
2014-08-22 11:48:03 +0100

    Output GC stats from UMIs

--- PipelineiCLIP.py
+++ PipelineiCLIP.py
@@ -1,6 +1,6 @@
 import CGAT.GTF as GTF
 import CGAT.IOTools as IOTools
-import CGAT.PipelineUtilities as PUtils
+import CGATPipelines.PipelineUtilities as PUtils
 import pandas
 import collections
 
@@ -18,7 +18,7 @@ def removeFirstAndLastExon(infile, outfile):
     outfile.close()
 
 
-def getBarcodeCG(table):
+def getBarcodeCG(table,outfile):
     ''' Annotate barcode use statistics with %GC '''
 
     statement = " SELECT * FROM %(table)s" % locals()
@@ -26,7 +26,15 @@ def getBarcodeCG(table):
     umi_stats = PUtils.fetch_DataFrame(statement)
 
     def _GC(x):
-        return (x.count("G") + x.count("G"))/len(x)
+        return float(x.count("G") + x.count("G"))/len(x)
 
-    barcode_gc = umi_stats.barcode.apply(_GC)
-    
+    barcode_gc = umi_stats.Barcode.apply(_GC)
+    sample_gc = umi_stats.Sample.apply(_GC)
+    umi_gc = umi_stats.UMI.apply(_GC)
+
+    gc_stats= pandas.DataFrame({"Barcode":umi_stats.Barcode,
+                                "barcode_gc": barcode_gc,
+                                "sample_gc": sample_gc,
+                                "umi_gc": umi_gc})
+
+    gc_stats.to_csv(IOTools.openFile(outfile,"w"), sep="\t", index=False)
