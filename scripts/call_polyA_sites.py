4b8338e305e3f934849ed9bded291384e608310b
Charlotte Vandermeulen <c.vandermeulen@sheffield.ac.uk>
2022-03-16 10:49:25 +0000

    debug neg index or over last index

--- scripts/call_polyA_sites.py
+++ scripts/call_polyA_sites.py
@@ -39,32 +39,34 @@ import pysam
 
 import iCLIP
 
-def find_peaks_in_contig(getter, 
-                         contig, 
+def find_peaks_in_contig(getter,
+                         contig,
                          strand,
                          window_width=10,
-                         threshold_height=2, 
+                         threshold_height=2,
                          threshold_sum=2,
                          dtype="int32"):
     '''This function performs the main algo. It scans the specified contig
-    finding the maximum point, records its position and signal, and then 
+    finding the maximum point, records its position and signal, and then
     groups it with the `width` bases either side, and removes them from the
-    signal. It returns a list of "peaks", each a list with the  with the 
+    signal. It returns a list of "peaks", each a list with the  with the
     following fields:
-    
+
     0: contig
     1: start (0-based)
     2: end (half-open)
     3: peak_height
     4: sum of signal in peak
     5: strand'''
-    
+
     results = list()
     contig0 = getter(contig, strand=strand, dtype=dtype)
     contig_sum = contig0.sum()
-    
+    last_index = contig0.last_valid_index()
+
     while contig_sum > 0:
         peak_location = contig0.idxmax()
+        #E.debug(print(peak_location))
         peak_start = peak_location - window_width
         peak_end = peak_location + window_width + 1
         peak = contig0.loc[peak_start:peak_end]
@@ -72,12 +74,19 @@ def find_peaks_in_contig(getter,
         peak_sum = peak.sum()
         contig0[peak_start:peak_end] = 0
         contig_sum = contig_sum - peak_sum
-    
+
+        if (peak_start < 0):
+            peak_start = 0
+        if (peak_end > last_index):
+            E.debug(peak_end)
+            peak_end = last_index + 1
+            E.debug(peak_end)
+
         if (peak_height <= threshold_height) or (
             peak_sum <= threshold_sum):
             continue
 
-        results.append([contig, int(peak_start), int(peak_end), 
+        results.append([contig, int(peak_start), int(peak_end),
                         peak_height, peak_sum, strand])
 
     return (results)
@@ -85,17 +94,17 @@ def find_peaks_in_contig(getter,
 def output_peak_to_bed(outfile, peak):
     '''Takes a peak line as output by find_peaks_in_contig and
     converts to bed format and outputs to the provided file'''
-    
+
     outbed = Bed.Bed()
     outbed.contig = peak[0]
     outbed.start = peak[1]
     outbed.end = peak[2]
-    outbed.name = peak[3]
-    outbed.score = peak[4]
-    outbed.strand = peak[5]
-    
+    outbed["name"] = peak[3]
+    outbed["score"] = peak[4]
+    outbed["strand"] = peak[5]
+
     outfile.write(str(outbed) + "\n")
-    
+
 def main(argv=None):
     """script main.
     parses command line options in sys.argv, unless *argv* is given.
@@ -103,7 +112,7 @@ def main(argv=None):
 
     if argv is None:
         argv = sys.argv
-    
+
     profiles = list(iCLIP.getters.profiles.keys())
 
     # setup command line parser
@@ -120,7 +129,7 @@ def main(argv=None):
                       default=10,
                       help="Number of bases to include either side of highest"
                            "point in peak")
-    
+
     # add common options (-h/--help, ...) and parse command line
     (options, args) = E.start(parser, argv=argv)
 
@@ -129,9 +138,9 @@ def main(argv=None):
                                 use_deletions=False,
                                  offset=0,
                                  filter_end="read1")
-    
+
     contigs = bamfile.references
-    
+
     out_peaks = list()
     for contig in contigs:
         results_plus = find_peaks_in_contig(polyA_signal, contig, "+",
@@ -144,15 +153,15 @@ def main(argv=None):
                                        threshold_sum=options.peak_sum)
         out_peaks.extend(results_plus)
         out_peaks.extend(results_minus)
-        E.debug(
-            "Done with contig %s, found %i peaks" % 
-            (contig, len(results_plus + results_minus)))
-        
-    E.debug("Starting output....")
-    
+        #E.debug(
+        #    "Done with contig %s, found %i peaks" %
+        #    (contig, len(results_plus + results_minus)))
+
+    #E.debug("Starting output....")
+
     for peak in out_peaks:
         output_peak_to_bed(options.stdout, peak)
-        
+
     E.stop()
 
 if __name__ == "__main__":
